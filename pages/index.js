8e604ee9fa2c36dfee7648fde4f74e90c4ac7cd8
Corey DeLoach <cdeloach16@gmail.com>
2022-01-24 09:09:50 -0600

    initial commit, setup

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Boiler | Styled</title>
+      </Head>
+
+      <h1>Title 1</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi soluta,
+        sequi tempora numquam quasi provident rerum itaque asperiores, excepturi
+        consequatur illo quae animi iure est quas nihil recusandae fuga iste.
+      </p>
+    </div>
+  );
+}
